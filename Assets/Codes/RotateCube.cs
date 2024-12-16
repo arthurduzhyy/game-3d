928da1576546cbd52ad1d783ec85c0f9cf05cb64
arthurduzhyy <artur.duzhyi@oa.edu.ua>
2024-12-16 22:15:01 +0200

    Scale and rotation animations

---
+++ Assets/Codes/RotateCube.cs
@@ -0,0 +1,43 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Codes
+{
+    public class RotateCube : MonoBehaviour
+    {
+        private const float DefaultAnimationDuration = 0f;
+
+        [SerializeField]
+        private GameObject cube;
+
+        [SerializeField]
+        private float duration = 2f;
+
+        [SerializeField]
+        private Ease easingCurve = Ease.InOutBounce;
+
+        private void OnEnable()
+        {
+            InputManager.OnRotateKeyPressed += RotateCubeOnKeyPress;
+        }
+
+        private void OnDisable()
+        {
+            InputManager.OnRotateKeyPressed -= RotateCubeOnKeyPress;
+        }
+
+        private void RotateCubeOnKeyPress()
+        {
+            cube.transform.DORotate(Vector3.zero, DefaultAnimationDuration);
+
+            var vector = new Vector3(0, 360, 0);
+
+            cube.transform
+                .DORotate(
+                    endValue: vector,
+                    duration: duration,
+                    mode: RotateMode.FastBeyond360)
+                .SetEase(easingCurve);
+        }
+    }
+}
---
+++ Assets/Codes/ScaleCube.cs
@@ -0,0 +1,51 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Codes
+{
+    public class ScaleCube : MonoBehaviour
+    {
+        private const int Vibrato = 1;
+        private const float Elasticity = 0f;
+
+        private Vector3 _originalScale;
+
+        [SerializeField]
+        private GameObject cube;
+
+        [SerializeField]
+        private float duration = 0.5f;
+
+        [SerializeField]
+        private Vector3 punchScale = new(0.4f, 0.4f, 0.4f);
+
+        [SerializeField]
+        private Ease easingStartCurve = Ease.InOutSine;
+        
+        [SerializeField]
+        private Ease easingEndCurve = Ease.OutBounce;
+        
+        private void OnEnable()
+        {
+            _originalScale = cube.transform.localScale;
+            InputManager.OnScaleKeyPressed += ScaleCubeOnKeyPress;
+        }
+
+        private void OnDisable()
+        {
+            InputManager.OnScaleKeyPressed -= ScaleCubeOnKeyPress;
+        }
+
+        private void ScaleCubeOnKeyPress()
+        {
+            cube.transform
+                .DOPunchScale(punchScale, duration, Vibrato, Elasticity)
+                .SetEase(easingStartCurve)
+                .OnComplete(() =>
+                {
+                    transform.DOScale(_originalScale, duration)
+                        .SetEase(easingEndCurve);
+                });
+        }
+    }
+}
